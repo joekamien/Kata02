b5379413fe2ae6d8a3ff29e3bc93125f529ae5e1
joekamien <joekamien@users.noreply.github.com>
2015-10-06 09:44:46 -0700

    Add chop function

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public class Main {
 
@@ -6,8 +7,29 @@ public class Main {
 	}
 
 	public static int chop(int target, int[] array){
+		if (array.length == 0){
+			return -1;
+		}else if (array.length == 1){
+			if (array[0] == target){
+				return 0;
+			}
+			else{
+				return -1;
+			}
+		}
 		
+		int halfwayPoint = array.length / 2;
+		int halfwayVal = array[halfwayPoint];
 		
-		return -1;
+		if (halfwayVal == target){
+			return halfwayPoint;
+		} else if (halfwayVal < target){
+			//generate new array from top half of array and run recursively
+			return halfwayPoint + chop(target, Arrays.copyOfRange(array, halfwayPoint, array.length - 1));
+		}
+		else{
+			//generate new array from bottom half of array and run recursively
+			return chop(target, Arrays.copyOfRange(array, 0, halfwayPoint));
+		}
 	}
 }
